9d7c23717d0f90e6fae9d9cd33efab2c635664ab
PabloAMartin <82456354+PabloAMartin@users.noreply.github.com>
2022-05-19 10:18:56 +0200

    tilemap placeholder and tilesets implementation

---
+++ tilemap/source/Tileset_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Tileset 2" tilewidth="32" tileheight="32" tilecount="480" columns="16">
+ <image source="../../Downloads/Pipoya RPG World Tileset 48x48 40x40 32x32 for Tiled1.5/Pipoya RPG World Tileset 48x48 40x40 32x32 for Tiled1.5/32x32/pipo-map001_at.png" width="512" height="960"/>
+</tileset>
