f79925330ef70560da849d35d704fd1d94f3e567
PabloAMartin <82456354+PabloAMartin@users.noreply.github.com>
2022-05-20 09:44:04 +0200

    room_01 map implementation

--- tilemap/source/Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Tileset" tilewidth="32" tileheight="32" tilecount="136" columns="8">
- <image source="../../Downloads/Pipoya RPG World Tileset 48x48 40x40 32x32 for Tiled1.5/Pipoya RPG World Tileset 48x48 40x40 32x32 for Tiled1.5/32x32/pipo-map001.png" width="256" height="544"/>
-</tileset>
---
+++ tilemap/tilesets/BgTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="BgTileset" tilewidth="32" tileheight="32" tilecount="480" columns="16">
+ <image source="../source/Pipoya RPG World Tileset/32x32/pipo-map001_at.png" width="512" height="960"/>
+</tileset>
