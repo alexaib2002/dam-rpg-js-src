180d22ebf623a4b1b2f849ddb7c09ad6e20aca2f
PabloAMartin <82456354+PabloAMartin@users.noreply.github.com>
2022-05-22 11:29:46 +0200

    Room_01, Room_02 & Room_03 implementation

---
+++ tilemap/tilesets/Angar1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Angar1" tilewidth="16" tileheight="16" tilecount="42" columns="6">
+ <image source="../source/MiniPack/angar2_blue.png" width="96" height="112"/>
+</tileset>
--- tilemap/tilesets/ObjTileset.tsx
+++ tilemap/tilesets/ObjTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="ObjTileset" tilewidth="32" tileheight="32" tilecount="136" columns="8">
- <image source="../source/Pipoya RPG World Tileset/32x32/pipo-map001.png" width="256" height="544"/>
+<tileset version="1.8" tiledversion="1.8.5" name="ObjTileset" tilewidth="16" tileheight="16" tilecount="42" columns="6">
+ <image source="../source/MiniPack/building3_blue.png" width="96" height="112"/>
 </tileset>
